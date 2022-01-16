e496d09c378fd63c39986738da4e076b66247b33
Your Name <you@example.com>
2022-01-16 19:43:06 +0300

    done2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+//import {products} from './products.js';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <h1 className="title">
          My First Saad1</h1>
 <p> This is my first simple website :)
+</p>
+<p>
+  S.K.A
 </p>
 < img className="pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJaNw5xaKkNmRDqQc7xbQqNZR31syN28laLA&usqp=CAU"  alt="sleepy"></img>
     </div>
---
+++ src/products.js
@@ -0,0 +1,13 @@
+products=[
+    {
+        name:"Red Pill",
+        Price:"10 KD" ,
+        image:" https://www.aljazeera.com/wp-content/uploads/2020/09/12c1dfc4aa49499a8cfd60ce55ce60de_18.jpeg?fit=1000%2C562",
+    },
+    {
+        name:"Blue Pill",
+        Price: "12 KD",
+        image:" https://incels.wiki/images/b/bb/Bluepill.png",
+    },
+]
+export default products;
